39728ca9e4a114eab210e06edeeed829cff9b038
Wang Bin <477982674@qq.com>
2015-07-01 11:43:40 +0800

    aa

---
+++ 123/123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  123
+//
+//  Created by tarena on 15/7/1.
+//  Copyright (c) 2015年 WB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 123/123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  123
+//
+//  Created by tarena on 15/7/1.
+//  Copyright (c) 2015年 WB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
